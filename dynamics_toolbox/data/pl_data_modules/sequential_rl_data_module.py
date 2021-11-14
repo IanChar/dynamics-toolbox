29f10871de0266192f287297af52518a6f80a7b5
IanChar <ianchar4@gmail.com>
2021-11-14 16:22:35 -0500

    Sets now split by trajectories for sequantial data.

--- dynamics_toolbox/data/pl_data_modules/sequential_rl_data_module.py
+++ dynamics_toolbox/data/pl_data_modules/sequential_rl_data_module.py
@@ -9,9 +9,9 @@ from typing import Dict, Union, List, Optional, Sequence
 import numpy as np
 import torch
 from pytorch_lightning import LightningDataModule
-from torch.utils.data import DataLoader, TensorDataset, random_split
+from torch.utils.data import DataLoader, TensorDataset
 
-from dynamics_toolbox.utils.gym_util import parse_into_trajectories
+from dynamics_toolbox.utils.sarsa_data_util import parse_into_snippet_datasets
 from dynamics_toolbox.utils.storage.qdata import get_data_from_source
 
 
@@ -21,7 +21,7 @@ class SequentialRlDataModule(LightningDataModule):
             self,
             data_source: str,
             batch_size: int,
-            nexts_are_deltas: bool = True,
+            te_data_source: Optional[str] = None,
             snippet_size: Optional[int] = None,
             val_proportion: float = 0.0,
             test_proportion: float = 0.0,
@@ -36,8 +36,7 @@ class SequentialRlDataModule(LightningDataModule):
             data_source: Name of the data source, either as a string to a path or
                 name of a d4rl env.
             batch_size: Batch size.
-            nexts_are_deltas: Whether the next observation should be a delta of
-                current observation.
+            te_data_source: Path to data source of test set.
             snippet_size: How big each snippet from a trajectory should be. If it is
                 None, set this to the smallest trajectory length.
             val_proportion: Proportion of data to use as validation.
@@ -48,50 +47,32 @@ class SequentialRlDataModule(LightningDataModule):
         """
         super().__init__()
         qset = get_data_from_source(data_source)
-        trajectories = parse_into_trajectories(qset)
-        traj_lengths = [len(traj['observations']) for traj in trajectories]
-        if snippet_size is None:
-            snippet_size = np.min(traj_lengths)
-        observations = []
-        actions = []
-        rewards = []
-        terminals = []
-        for traj, traj_len in zip(trajectories, traj_lengths):
-            for idx in range(traj_len + 1 - snippet_size):
-                observations.append(np.vstack([
-                    traj['observations'][[idx]],
-                    traj['next_observations'][idx:idx + snippet_size]
-                ]))
-                actions.append(traj['actions'][idx:idx + snippet_size])
-                rewards.append(traj['rewards'][idx:idx + snippet_size])
-                terminals.append(traj['terminals'][idx:idx + snippet_size])
-        self._observations = np.array(observations)
-        self._actions = np.array(actions)
-        self._rewards = np.array(rewards)
-        self._terminals = np.array(terminals)
-        self._val_proportion = val_proportion
-        self._test_proportion = test_proportion
+        self._snippets = parse_into_snippet_datasets(
+            qset,
+            snippet_size=snippet_size,
+            val_proportion=val_proportion,
+            test_proportion=test_proportion,
+        )
+        if te_data_source is not None:
+            te_qset = get_data_from_source(te_data_source)
+            self._snippets[-1] = parse_into_snippet_datasets(
+                te_qset,
+                snippet_size=snippet_size,
+                val_proportion=0,
+                test_proportion=0,
+            )[0]
+        self._tr_dataset, self._val_dataset, self._te_dataset = [
+            TensorDataset(
+                torch.Tensor(ds['observations']),
+                torch.Tensor(ds['actions']),
+                torch.Tensor(ds['next_observations'] - ds['observations']),
+                torch.Tensor(ds['rewards']),
+                torch.Tensor(ds['terminals'])
+            ) for ds in self._snippets]
         self._batch_size = batch_size
         self._num_workers = num_workers
         self._pin_memory = pin_memory
         self._seed = seed
-        data_size = len(self._observations)
-        self._num_val = int(data_size * val_proportion)
-        self._num_te = int(data_size * test_proportion)
-        self._num_tr = data_size - self._num_val - self._num_te
-        self._nexts = self._observations[:, 1:]
-        if nexts_are_deltas:
-            self._nexts = self._nexts - self._observations[:, :-1]
-        self._tr_dataset, self._val_dataset, self._te_dataset = random_split(
-            TensorDataset(
-                torch.Tensor(self._observations[:, :-1]),
-                torch.Tensor(self._actions),
-                torch.Tensor(self._nexts),
-                torch.Tensor(self._rewards),
-                torch.Tensor(self._terminals),
-            ),
-            [self._num_tr, self._num_val, self._num_te],
-        )
 
     def train_dataloader(
             self,
@@ -141,40 +122,50 @@ class SequentialRlDataModule(LightningDataModule):
     @property
     def data(self) -> Sequence[np.array]:
         """Get all of the data."""
-        return (self._observations[:, :-1], self._actions, self._nexts,
-                self._rewards, self._terminals)
+        return (
+            self._snippets[0]['observations'],
+            self._snippets[0]['actions'],
+            self._snippets[0]['next_observations'] - self._snippets[0]['observations'],
+            self._snippets[0]['rewards'],
+            self._snippets[0]['terminals'],
+        )
 
     @property
     def input_data(self) -> np.array:
         """The input data.."""
-        return np.concatenate([self._observations[:, :-1], self._actions], axis=-1)
+        return np.concatenate([
+            self._snippets[0]['observations'],
+            self._snippets[0]['actions'],
+        ], axis=-1)
 
     @property
     def output_data(self) -> np.array:
         """The output data."""
-        return self._nexts
+        return self._snippets[0]['next_observations'] - self._snippets[0][
+            'observations']
 
     @property
     def input_dim(self) -> int:
         """Input dimension."""
-        return self._observations.shape[-1] + self._actions.shape[-1]
+        return self._snippets[0]['observations'].shape[-1] \
+               + self._snippets[0]['actions'].shape[-1]
 
     @property
     def output_dim(self) -> int:
         """Output dimension."""
-        return self._observations.shape[-1]
+        return self._snippets[0]['next_observations'].shape[-1]
 
     @property
     def num_train(self) -> int:
         """Number of training points."""
-        return self._num_tr
+        return self._snippets[0]['observations'].shape[0]
 
     @property
     def num_validation(self) -> int:
         """Number of training points."""
-        return self._num_val
+        return self._snippets[1]['observations'].shape[0]
 
     @property
     def num_test(self) -> int:
         """Number of training points."""
-        return self._num_te
+        return self._snippets[2]['observations'].shape[0]
---
+++ dynamics_toolbox/utils/sarsa_data_util.py
@@ -0,0 +1,93 @@
+"""
+Utility for gym environments and gym data.
+
+Author: Ian Char
+"""
+from typing import Dict, List, Optional, Sequence
+
+import numpy as np
+
+
+def parse_into_trajectories(
+        dataset: Dict[str, np.ndarray],
+) -> List[Dict[str, np.ndarray]]:
+    """Parse a dataset into list of datasets separated by trajectory.
+
+    Args:
+        dataset: Full dataset with multiple trajectories. The dataset at least has
+            "observations", "actions", "next_observations", "rewards", "terminals".
+
+    Returns:
+        List of datasets where each one is a trajectory.
+    """
+    trajectories = []
+    start_idx = 0
+    for end_idx in range(1, dataset['observations'].shape[0]):
+        if end_idx == dataset['observations'].shape[0] - 1 or not np.allclose(
+            dataset['next_observations'][end_idx - 1],
+            dataset['observations'][end_idx],
+        ):
+            trajectories.append({k: v[start_idx:end_idx]
+                                 for k, v in dataset.items()})
+            start_idx = end_idx
+    return trajectories
+
+
+def parse_into_snippet_datasets(
+        qset: Dict[str, np.ndarray],
+        snippet_size: Optional[int] = None,
+        val_proportion: float = 0.0,
+        test_proportion: float = 0.0,
+) -> List[Dict[str, np.ndarray]]:
+    """Parse into a sarsa dataset into snippets of rollouts.
+
+    Args:
+        qset: Full dataset with multiple trajectories. The dataset at least has
+            "observations", "actions", "next_observations", "rewards", "terminals".
+        snippet_size: The length of the snippet size. If left as None, this will
+            default to the smallest trajectory size in the dataset.
+        val_proportion: Proportion of the trajectories to be used for validation.
+        test_proportion: Proportion of the trajectories to be used for testing.
+
+    Returns:
+        Three dictionaries, one for each train, validation, and testing. Each contains
+            * observations: ndarray of shape (num_snippets, snippet_size, obs_dim)
+            * actions: ndarray of shape (num_snippets, snippet_size, act_dim)
+            * next_observations: ndarray of shape (num_snippets, snippet_size, obs_dim)
+            * rewards: ndarray of shape (num_snippets, snippet_size)
+            * terminals: ndarray of shape (num_snippets, snippet_size)
+    """
+    if val_proportion + test_proportion > 1:
+        raise ValueError('Invalid validation and test proportions: '
+                         f'{val_proportion} and {test_proportion}.')
+    trajectories = parse_into_trajectories(qset)
+    np.random.shuffle(trajectories)
+    min_trajectory_length = np.min([len(traj['observations']) for traj in trajectories])
+    if snippet_size is None:
+        snippet_size = min_trajectory_length
+    else:
+        if snippet_size > min_trajectory_length:
+            raise ValueError(f'Cannot have snippet size ({snippet_size} greater '
+                             f'than min trajectory length ({min_trajectory_length})')
+    num_te = int(test_proportion * len(trajectories))
+    te_trajectories, trajectories = trajectories[:num_te], trajectories[num_te:]
+    num_val = int(val_proportion * len(trajectories))
+    val_trajectories, trajectories = trajectories[:num_val], trajectories[num_val:]
+    datasets = [{
+        'observations': [],
+        'actions': [],
+        'next_observations': [],
+        'rewards': [],
+        'terminals': [],
+    } for _ in range(3)]
+    for dataset, trajectory_group in zip(datasets,
+                                         [trajectories, val_trajectories,
+                                          te_trajectories]):
+        for traj in trajectory_group:
+            traj_len = len(traj)
+            for idx in range(traj_len + 1 - snippet_size):
+                for k, arr in dataset.items():
+                    arr.append(traj[k][idx:idx + snippet_size])
+        for k, arr in dataset.items():
+            dataset[k] = np.array(arr)
+    return datasets

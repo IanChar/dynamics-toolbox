10dbeb5d166567902055897fd27d814706f52b73
IanChar <ianchar4@gmail.com>
2021-11-17 11:34:37 -0500

    Removed get val loss.

--- dynamics_toolbox/models/pl_models/quantile_models.py
+++ dynamics_toolbox/models/pl_models/quantile_models.py
@@ -120,51 +120,6 @@ class QuantileModel(AbstractPlModel):
         assert q_pred.shape == (num_pts, num_q)
         return q_pred
 
-    def orig_forward(
-        self,
-        x: torch.Tensor,
-        q_list: torch.Tensor = None,
-        recal_model: Any = None,
-        recal_type: str = None,
-    ) -> torch.Tensor:
-        """Get output for given list of quantiles
-
-        Args:
-            x: tensor, of size (num_x, dim_x)
-            q_list: flat tensor of quantiles, if None, is set to [0.01, ..., 0.99]
-            recal_model: a recalibration model #TODO
-            recal_type: recalibration type  #TODO
-
-        Returns:
-            Given N input (x) points and K quantiles, outputs a NxK tensor
-        """
-        if q_list is None:
-            q_list = torch.linspace(0.01, 0.99, 99)
-        else:
-            q_list = q_list.flatten()
-        num_x = x.shape[0]
-        num_q = q_list.shape[0]
-        cdf_preds = []
-        for p in q_list:
-            if recal_model is not None:
-                if recal_type == "torch":
-                    recal_model.cpu()  # keep recal model on cpu
-                    with torch.no_grad():
-                        in_p = recal_model(p.reshape(1, -1)).item()
-                elif recal_type == "sklearn":
-                    in_p = float(recal_model.predict(p.flatten()))
-                else:
-                    raise ValueError("recal_type incorrect")
-            else:
-                in_p = float(p)
-            p_tensor = (in_p * torch.ones(num_x)).reshape(-1, 1)
-            cdf_in = torch.cat([x, p_tensor], dim=1).to(self.device)
-            cdf_pred = self._quantile_network(cdf_in)  # shape (num_x, 1)
-            cdf_preds.append(cdf_pred)
-        pred_mat = torch.cat(cdf_preds, dim=1)  # shape (num_x, num_q)
-        assert pred_mat.shape == (num_x, num_q)
-        return pred_mat
-
     def get_net_out(self, batch: Sequence[torch.Tensor]) -> Dict[str, torch.Tensor]:
         """Get the output of the network and organize into dictionary.
 
@@ -179,7 +134,8 @@ class QuantileModel(AbstractPlModel):
         q_pred = self.forward(x=xi, q_list=q_list, recal_model=None, recal_type=None)
         return {'q_list': q_list, 'q_pred': q_pred}
 
-    def get_val_net_out(self, batch: Sequence[torch.Tensor]) -> Dict[str, torch.Tensor]:
+    def get_eval_net_out(self, batch: Sequence[torch.Tensor]) \
+            -> Dict[str, torch.Tensor]:
         """Get the validation output of the network and organize into dictionary.
 
         Args:
@@ -213,29 +169,6 @@ class QuantileModel(AbstractPlModel):
         xi, yi = batch
         q_list = net_out['q_list']
         q_pred = net_out['q_pred']
-        return self._compute_quantile_model_loss_stats(
-            xi=xi, yi=yi, q_pred=q_pred, q_list=q_list
-        )
-
-    def val_loss(
-            self,
-            net_out: Dict[str, torch.Tensor],
-            batch: Sequence[torch.Tensor],
-    ) -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
-        """Compute the validation loss function.
-
-        Args:
-            net_out: The output of the network, which for this model will be empty dict.
-            batch: The batch passed into the network.
-
-        Returns:
-            The loss and a dictionary of other statistics.
-        """
-        xi, yi = batch
-        q_list = net_out['q_list']
-        q_pred = net_out['q_pred']
-        assert q_list.shape == (99,)
-        assert torch.max(q_list - torch.linspace(0.01, 0.99, 99)) < 1e-10
         return self._compute_quantile_model_loss_stats(
             xi=xi, yi=yi, q_pred=q_pred, q_list=q_list
         )

923ff9b4d088cb3a70d7989b8742d25a8b50ac55
Youngseog Chung <youngsec@cs.cmu.edu>
2021-11-17 11:55:09 -0500

    final fixes for quantile model

--- dynamics_toolbox/models/pl_models/abstract_pl_model.py
+++ dynamics_toolbox/models/pl_models/abstract_pl_model.py
@@ -53,7 +53,7 @@ class AbstractPlModel(LightningModule, AbstractModel, metaclass=abc.ABCMeta):
     def validation_step(self, batch: Sequence[torch.Tensor], batch_idx: int) -> None:
         """Training step for pytorch lightning. Returns the loss."""
         batch = self.normalizer.normalize_batch(batch)
-        net_out = self.get_val_net_out(batch)
+        net_out = self.get_eval_net_out(batch)
         loss, loss_dict = self.loss(net_out, batch)
         loss_dict.update(self._get_test_and_validation_metrics(net_out, batch))
         self._log_stats(loss_dict, prefix='val')
@@ -61,7 +61,7 @@ class AbstractPlModel(LightningModule, AbstractModel, metaclass=abc.ABCMeta):
     def test_step(self, batch: Sequence[torch.Tensor], batch_idx: int) -> None:
         """Training step for pytorch lightning. Returns the loss."""
         batch = self.normalizer.normalize_batch(batch)
-        net_out = self.get_val_net_out(batch)
+        net_out = self.get_eval_net_out(batch)
         loss, loss_dict = self.loss(net_out, batch)
         loss_dict.update(self._get_test_and_validation_metrics(net_out, batch))
         self._log_stats(loss_dict, prefix='test')
@@ -97,7 +97,7 @@ class AbstractPlModel(LightningModule, AbstractModel, metaclass=abc.ABCMeta):
             weight_decay=self.weight_decay,
         )
 
-    def get_val_net_out(self, batch: Sequence[torch.Tensor]) -> Dict[str, torch.Tensor]:
+    def get_eval_net_out(self, batch: Sequence[torch.Tensor]) -> Dict[str, torch.Tensor]:
         """Get the validation output of the network and organize into dictionary.
 
         Args:

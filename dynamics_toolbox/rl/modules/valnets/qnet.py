7bb0a707f70fe9a5e30476601524dc483d67a308
Ian Char <ianchar4@gmail.com>
2023-04-13 19:46:21 -0400

    Added sequential qnet.

--- dynamics_toolbox/rl/modules/valnets/qnet.py
+++ dynamics_toolbox/rl/modules/valnets/qnet.py
@@ -4,12 +4,16 @@ A standard Q network.
 Author: Ian Char
 Date: April 6, 2023
 """
-from typing import Callable, Sequence
+from typing import Callable, Optional, Sequence
 
 import torch
 from torch import Tensor
+import torch.nn as nn
 from torch.nn import functional as F
 
+from dynamics_toolbox.rl.modules.history_encoders.abstract_history_encoder import (
+    HistoryEncoder,
+)
 from dynamics_toolbox.utils.pytorch.modules.fc_network import FCNetwork
 
 
@@ -21,6 +25,7 @@ class QNet(FCNetwork):
         act_dim: int,
         hidden_sizes: Sequence[int],
         hidden_activation: Callable[Tensor, Tensor] = F.relu,
+        input_dim: Optional[int] = None,
         **kwargs
     ):
         """Constructor.
@@ -31,11 +36,13 @@ class QNet(FCNetwork):
         hidden_activation: Activation function.
         """
         super().__init__(
-            input_dim=obs_dim + act_dim,
+            input_dim=obs_dim + act_dim if input_dim is None else input_dim,
             output_dim=1,
             hidden_sizes=hidden_sizes,
             hidden_activation=hidden_activation,
         )
+        self._obs_dim = obs_dim
+        self._act_dim = act_dim
 
     def forward(self, obs: Tensor, act: Tensor) -> Tensor:
         """Forward pass.
@@ -47,3 +54,50 @@ class QNet(FCNetwork):
         Returns: Value of shape (batch_size, 1)
         """
         return super().forward(torch.cat([obs, act], dim=-1))
+
+
+class SequentialQNet(QNet):
+
+    def __init__(
+        self,
+        history_encoder: HistoryEncoder,
+        obs_act_encode_dim: int,
+        **kwargs
+    ):
+        """Constructor.
+
+        Args:
+            history_encoder: Encoder for the history.
+            obs_act_encode_dim: The size of the encoding for joint obs, act encoding.
+            All the rest are the same as QNet.
+        """
+        super().__init__(
+            input_dim=history_encoder.out_dim + obs_act_encode_dim,
+            **kwargs
+        )
+        self._history_encoder = history_encoder
+        self._obs_act_encoder = nn.Linear(self._obs_dim + self._act_dim,
+                                          obs_act_encode_dim)
+
+    def forward(
+        self,
+        obs_seq: Tensor,
+        prev_act_seq: Tensor,
+        rew_seq: Tensor,
+        act_seq: Tensor,
+    ) -> Tensor:
+        """
+        Forward pass. Where B = batch_size, L = seq length..
+
+        Args:
+            obs_seq: (B, L, obs_dim)
+            prev_act_seq: (B, L, act_dim)
+            rew_seq: (B, L, 1)
+            act_seq: (B, L, act_dim)
+
+        Returns: Tensor
+        """
+        encoding = self._history_encoder(obs_seq, prev_act_seq, rew_seq)[0]
+        obs_act_encoding = self.hidden_activation(self._obs_act_encoder(
+            torch.cat([obs_seq, act_seq]), dim=-1))
+        return super().forward(torch.cat([encoding, obs_act_encoding], dim=-1))

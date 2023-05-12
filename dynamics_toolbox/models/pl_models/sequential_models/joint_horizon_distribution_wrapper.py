2ffecd283ec68b450ca9be6ddbc5950a2e6822a8
YoungseogChung <youngsec@andrew.cmu.edu>
2023-05-11 20:12:03 -0400

    fixes to joint_horizon_distribution_wrapper for torch

--- dynamics_toolbox/models/pl_models/sequential_models/joint_horizon_distribution_wrapper.py
+++ dynamics_toolbox/models/pl_models/sequential_models/joint_horizon_distribution_wrapper.py
@@ -8,8 +8,9 @@ Date: May 8, 2023
 import os
 from typing import Dict
 
-import torch
+import tqdm
 import numpy as np
+import torch
 
 
 def batch_conditional_sampling_with_joint_correlation(
@@ -63,23 +64,23 @@ def batch_conditional_sampling_with_joint_correlation(
               num_hist:(num_hist + num_pred), :]
             * pred_stds.unsqueeze(2))
 
-    torch.allclose(hist_pred_cov, pred_hist_cov.transpose(0, 2, 1, 3))
+    torch.allclose(hist_pred_cov, pred_hist_cov.permute(0, 2, 1, 3))
     assert hist_block_cov.shape == (num_samples, num_hist, num_hist, obs_dim)
     assert pred_hist_cov.shape == (num_samples, num_pred, num_hist, obs_dim)
     assert hist_pred_cov.shape == (num_samples, num_hist, num_pred, obs_dim)
     assert pred_pred_cov.shape == (num_samples, num_pred, num_pred, obs_dim)
 
-    hist_block_cov = hist_block_cov.transpose(0, 3, 1,
+    hist_block_cov = hist_block_cov.permute(0, 3, 1,
                                               2)  # (num_samples, obs_dim, num_hist, num_hist)
-    pred_hist_cov = pred_hist_cov.transpose(0, 3, 1,
+    pred_hist_cov = pred_hist_cov.permute(0, 3, 1,
                                             2)  # (num_samples, obs_dim, num_pred, num_hist)
-    hist_pred_cov = hist_pred_cov.transpose(0, 3, 1,
+    hist_pred_cov = hist_pred_cov.permute(0, 3, 1,
                                             2)  # (num_samples, obs_dim, num_hist, num_pred)
-    pred_pred_cov = pred_pred_cov.transpose(0, 3, 1,
+    pred_pred_cov = pred_pred_cov.permute(0, 3, 1,
                                             2)  # (num_samples, obs_dim, num_pred, num_pred)
-    hist_obs = hist_obs.transpose(0, 2, 1)  # (num_samples, obs_dim, num_hist)
-    hist_means = hist_means.transpose(0, 2, 1)  # (num_samples, obs_dim, num_hist)
-    pred_means = pred_means.transpose(0, 2, 1)  # (num_samples, obs_dim, num_pred)
+    hist_obs = hist_obs.permute(0, 2, 1)  # (num_samples, obs_dim, num_hist)
+    hist_means = hist_means.permute(0, 2, 1)  # (num_samples, obs_dim, num_hist)
+    pred_means = pred_means.permute(0, 2, 1)  # (num_samples, obs_dim, num_pred)
 
     assert hist_block_cov.shape == (num_samples, obs_dim, num_hist, num_hist)
     assert pred_hist_cov.shape == (num_samples, obs_dim, num_pred, num_hist)
@@ -120,57 +121,62 @@ def batch_conditional_sampling_with_joint_correlation(
     assert cond_cov.shape == (num_samples, obs_dim, num_pred, num_pred)
 
     cond_samples_np = np.zeros((num_samples, obs_dim, num_pred, num_draw_per_rv))
-    cond_samples = torch.zeros((num_samples, obs_dim, num_pred, num_draw_per_rv))
+    # cond_samples = torch.zeros((num_samples, obs_dim, num_pred, num_draw_per_rv))
 
-    ###
-    # TODO: I have to iterate over sample_idx and obs_idx because numpy's multivariate_normal only supports 2D inputs
-    mean_list = []
-    cov_list = []
-    for b in range(num_samples):
-        mean_list_per_sample = []
-        cov_list_per_sample = []
-        for i in range(obs_dim):
-            dim_samples = np.random.multivariate_normal(cond_mean[b, i], cond_cov[b, i],
-                                                        size=num_draw_per_rv).T  # (num_pred, num_draw_per_rv)
-            cond_samples_np[b, i] = dim_samples
-            mean_list_per_sample.append(cond_mean[b, i])
-            cov_list_per_sample.append(cond_cov[b, i])
-        mean_list.append(np.stack(mean_list_per_sample, axis=-1))  # (num_pred, obs_dim)
-        cov_list.append(
-            np.stack(cov_list_per_sample, axis=-1))  # (num_pred, num_pred, obs_dim)
-    mean_list = np.stack(mean_list, axis=0)  # (num_samples, num_pred, obs_dim)
-    cov_list = np.stack(cov_list, axis=0)  # (num_samples, num_pred, num_pred, obs_dim)
-    ###
     ### BEGIN: new code
-    cond_mean = torch.from_numpy(cond_mean)
-    cond_cov = torch.from_numpy(cond_cov)
+    # breakpoint()
     # for b in range(num_samples):
     mean_list_per_dim = []
     cov_list_per_dim = []
+    cond_samples_per_dim = []
     for i in range(obs_dim):
         # dim_samples = np.random.multivariate_normal(cond_mean[b, i], cond_cov[b, i],
         #                                             size=num_draw_per_rv).T  # (num_pred, num_draw_per_rv)
         dim_distrs = torch.distributions.MultivariateNormal(cond_mean[:, i, :], cond_cov[:, i, :, :])
-        dim_samples = dim_distrs.sample((num_draw_per_rv,)).T  # (num_pred, num_draw_per_rv)
-        assert dim_samples.shape == (num_pred, num_draw_per_rv)
-        cond_samples[:, i] = dim_samples
+        dim_samples = dim_distrs.sample((num_draw_per_rv,)).permute(1, 2, 0)  
+        assert dim_samples.shape == (num_samples, num_pred, num_draw_per_rv)
+        # cond_samples[:, i] = dim_samples
+        cond_samples_per_dim.append(dim_samples)
         mean_list_per_dim.append(cond_mean[:, i, :])
         cov_list_per_dim.append(cond_cov[:, i, :, :])
-    torch_mean_list = torch.stack(mean_list_per_dim, dim=-1)  # (num_pred, obs_dim)
-    torch_cov_list = torch.stack(cov_list_per_dim, dim=-1)  # (num_pred, num_pred, obs_dim)
+    cond_samples = torch.stack(cond_samples_per_dim, dim=1)
+    mean_list = torch.stack(mean_list_per_dim, dim=-1)  # (num_pred, obs_dim)
+    cov_list = torch.stack(cov_list_per_dim, dim=-1)  # (num_pred, num_pred, obs_dim)
     ###
+    # ###
+    # print('old_code')
+    # mean_list = []
+    # cov_list = []
+    # # I have to iterate over sample_idx and obs_idx because numpy's multivariate_normal only supports 2D inputs
+    # for b in tqdm.tqdm(range(num_samples)):
+    #     mean_list_per_sample = []
+    #     cov_list_per_sample = []
+    #     for i in range(obs_dim):
+    #         dim_samples = np.random.multivariate_normal(cond_mean[b, i].cpu().numpy(), cond_cov[b, i].cpu().numpy(),
+    #                                                     size=num_draw_per_rv).T  # (num_pred, num_draw_per_rv)
+    #         cond_samples_np[b, i] = dim_samples
+    #         mean_list_per_sample.append(cond_mean[b, i].cpu().numpy())
+    #         cov_list_per_sample.append(cond_cov[b, i].cpu().numpy())
+    #     mean_list.append(np.stack(mean_list_per_sample, axis=-1))  # (num_pred, obs_dim)
+    #     cov_list.append(
+    #         np.stack(cov_list_per_sample, axis=-1))  # (num_pred, num_pred, obs_dim)
+    # mean_list = np.stack(mean_list, axis=0)  # (num_samples, num_pred, obs_dim)
+    # cov_list = np.stack(cov_list, axis=0)  # (num_samples, num_pred, num_pred, obs_dim)
+    # ###
 
-    breakpoint()
-    check_mean = torch_mean_list.cpu().numpy()
-    check_cov = torch_cov_list.cpu().numpy()
+    # breakpoint()
+    # check_mean = torch_mean_list.cpu().numpy()
+    # check_cov = torch_cov_list.cpu().numpy()
 
 
 
 
-    out_samples = cond_samples.transpose(0, 2, 3,
+    out_samples = cond_samples.permute(0, 2, 3,
                                          1)  # (num_samples, num_pred, num_draw_per_rv, obs_dim)
-    out_means = np.stack(mean_list, axis=0)
-    out_covs = np.stack(cov_list, axis=0)
+    out_means = mean_list
+    out_covs = cov_list
+    # out_means = np.stack(mean_list, axis=0)
+    # out_covs = np.stack(cov_list, axis=0)
 
     assert out_means.shape == (num_samples, num_pred, obs_dim)
     assert out_covs.shape == (num_samples, num_pred, num_pred, obs_dim)
@@ -269,7 +275,7 @@ class joint_horizon_distribution_wrapper():
     def predict(self, model_input, **kwargs):
         if not self.updated_model_device:
             self.model_device = getattr(self.wrapped_model.normalizer, '1_scaling').device
-            self.error_corr_mat = self.error_corr_mat.to(self.model_device)
+            self.error_corr_mat = torch.from_numpy(self.error_corr_mat).to(self.model_device)
             if self.apply_recal:
                 self.recal_constants = self.recal_constants.to(self.model_device)
             self.updated_model_device = True
@@ -311,15 +317,15 @@ class joint_horizon_distribution_wrapper():
             assert hist_obs.shape[1:] == (self.h_idx, self.obs_dim)
 
             gamma, _ = batch_conditional_sampling_with_joint_correlation(
-                corr_mat=self.error_corr_mat[1:, 1:],
-                hist_obs=hist_obs,
-                hist_means=torch.zeros((batch_size, self.h_idx, self.obs_dim), device=self.model_device),
-                hist_stds=torch.ones((batch_size, self.h_idx, self.obs_dim), device=self.model_device),
-                pred_means=torch.zeros((batch_size, 1, self.obs_dim), device=self.model_device),
-                pred_stds=torch.ones((batch_size, 1, self.obs_dim), device=self.model_device),
+                corr_mat=self.error_corr_mat[1:, 1:].float(),
+                hist_obs=hist_obs.float(),
+                hist_means=torch.zeros((batch_size, self.h_idx, self.obs_dim), device=self.model_device).float(),
+                hist_stds=torch.ones((batch_size, self.h_idx, self.obs_dim), device=self.model_device).float(),
+                pred_means=torch.zeros((batch_size, 1, self.obs_dim), device=self.model_device).float(),
+                pred_stds=torch.ones((batch_size, 1, self.obs_dim), device=self.model_device).float(),
                 num_draw_per_rv=1
             )
-            gamma = torch.from_numpy(gamma[:, 0, 0, :])
+            gamma = gamma[:, 0, 0, :]
         assert obs_delta_mean_preds.shape == gamma.shape == std_preds.shape, "shapes of obs_delta_mean_preds, gamma, and std_preds must match"
         if self.apply_recal:
             use_std = std_preds * self.recal_constants[self.h_idx]
@@ -330,8 +336,8 @@ class joint_horizon_distribution_wrapper():
         ### BEGIN: new code block
         obs_delta = self.wrapped_model._unnormalize_prediction_output(obs_delta).cpu().numpy()
         ### END: new code block
-        info = {'mean_predictions': obs_delta_mean_preds,  # this is non-normalized
-                'std_predictions': gamma * use_std}  # this is non-normalized
+        info = {'mean_predictions': obs_delta_mean_preds.cpu().numpy(),  # this is non-normalized
+                'std_predictions': (gamma * use_std).cpu().numpy()}  # this is non-normalized
 
         self.observed_gamma.append(gamma)
         self.h_idx += 1

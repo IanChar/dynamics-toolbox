d71cc3f47dbcc37b338585446ea9f9cc07d8f3cf
Rohit Sonker <rsonker@gpu3.int.autonlab.org>
2024-04-29 16:23:38 -0400

    added simple FF

--- dynamics_toolbox/models/pl_models/classification_models/SimpleTearingProbabilityModel.py
+++ dynamics_toolbox/models/pl_models/classification_models/SimpleTearingProbabilityModel.py
@@ -1,5 +1,5 @@
 """
-Classification model developed by Jaemin et al for predicting tearing instability in Plasma
+Model with ff layers only. Trained on PCA and normed data, so should not require preprocessing for prediction.
 Taken from TearingAvoidance Repo. Model is written in Keras.
 Preprocessing is defined in the reward function. This just predicts for preprocessed data.
 
@@ -9,8 +9,9 @@ Author: Rohit Sonker
 from typing import Tuple, Dict, Any
 import numpy 
 import tf_keras as keras
+import os
 
-class PPPLTearingProbabilityModel():
+class FFTearingProbabilityModel():
     def __init__(self,  
                  model_path:str, #= "/zfsauton2/home/rsonker/TearingAvoidance/tm_prediction_model",
                  is_ensemble:bool = True,
@@ -21,22 +22,23 @@ class PPPLTearingProbabilityModel():
         if self.is_ensemble:
             self.model = []
             for seed in range(5):
-                model = keras.saving.load_model(f'{self.model_path}/best_model_{seed}',
+                model = keras.saving.load_model(os.path.join(model_path,f'best_model_{seed}'),
                                                 compile=False)
                 self.model.append(model)
         else:
-            self.model = keras.saving.load_model(f'{self.model_path}/best_model_{use_model_seed}', compile = False)
+            self.model = keras.saving.load_model(os.path.join(model_path,f'best_model_{use_model_seed}'), compile = False)
 
     
     def predict(self, x0: numpy.ndarray, x1:numpy.ndarray, combine_predictions = 'mean') -> numpy.ndarray:
+        x = numpy.hstack([x0, x1])
         if self.is_ensemble:
             predictions = []
             for model in self.model:
-                predictions.append(model.predict([x0,x1], verbose=0))
+                predictions.append(model.predict(x, verbose=0))
             predictions = numpy.array(predictions)
             return numpy.mean(predictions, axis=0)
         else:
-            return self.model.predict([x0, x1], verbose = 0)
+            return self.model.predict(x, verbose = 0)
         
 
 
